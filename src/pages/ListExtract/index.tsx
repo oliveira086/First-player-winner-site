bcd2696a68fd55c22ecfc7a13912008a20417925
Vitor Rafael Rocha Campelo <vitorafael.r.c@gmail.com>
2021-04-21 00:31:45 -0300

    Creating money props cards

--- src/pages/ListExtract/index.tsx
+++ src/pages/ListExtract/index.tsx
@@ -32,11 +32,33 @@ const Icon = styled.img`
   margin-right: 9px;
 `;
 
+const CardContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  margin-bottom: 10px;
+  margin-top: 30px;
+  padding-left: 30px;
+  flex-direction: row;
+  overflow-x: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
 const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 9px 9px 5px 9px;
+  flex: none;
   width: 100px;
   height: 60px;
   border-radius: 5px;
+  margin-right: 20px;
   background-color: rgba(210, 209, 211, 0.6);
+  font-size: 12px;
+  line-height: 12.2px;
+  font-weight: 500;
+  justify-content: flex-end;
 `;
 
 const ListExtract: React.FC = () => {
@@ -49,7 +71,11 @@ const ListExtract: React.FC = () => {
         <br />
         <Strong>$ 100,00</Strong>
       </BalanceHeader>
-      <Card />
+      <CardContainer>
+        <Card>Depositar</Card>
+        <Card>Transferir</Card>
+        <Card>Solicitar {'\n'} saque</Card>
+      </CardContainer>
       <Hr />
     </Container>
   );
