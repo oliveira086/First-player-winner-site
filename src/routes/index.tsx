02301fff95b74a0489de6e1a75e5384ee4195d3e
Pedro Neto <netopedropgn@gmail.com>
2021-05-01 14:31:02 -0300

    feat: adding registration screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import PreLoginPage from '../pages/PreLoginPage';
 import LoginPage from '../pages/LoginPage';
+import RegistrationPage from '../pages/RegistrationPage';
 import ListChampionships from '../pages/ListChampionships';
 import ChampionshipDetails from '../pages/ChampionshipsDetails';
 import ListExtract from '../pages/ListExtract';
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={PreLoginPage} />
     <Route path="/loginPage" exact component={LoginPage} />
+    <Route path="/registrationPage" exact component={RegistrationPage} />
     <Route path="/listchampionship" exact component={ListChampionships} />
     <Route path="/championship/details" exact component={ChampionshipDetails} />
     <Route path="/extract" exact component={ListExtract} />
