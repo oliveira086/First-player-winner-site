cc119c05c36c4e7ab4db7161b02196517b5e04a6
Vitor <vitorafael.r.c@gmail.com>
2021-06-08 22:56:21 -0300

    feat: award input

--- src/components/atoms/CreateChampionship/SelectInput/index.tsx
+++ src/components/atoms/CreateChampionship/SelectInput/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
-import styled from 'styled-components';
+import React, { InputHTMLAttributes, useState } from 'react';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+import styled, { css } from 'styled-components';
 
 interface SelectProps extends InputHTMLAttributes<HTMLInputElement> {
   value: string;
@@ -13,11 +14,12 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  margin-bottom: 30px;
 `;
 
-const Select = styled.select`
+const Select = styled.div`
   width: calc(100vw - 60px);
-  height: 40px;
+  height: auto;
   border: 2px solid #020c28;
   border-radius: 10px;
   background-color: inherit;
@@ -26,12 +28,43 @@ const Select = styled.select`
   font-size: 12px;
   font-weight: 500;
   position: relative;
+  padding-top: 30px;
+  padding-bottom: 12px;
 `;
 
-const Viadagem = styled.div`
+const InputContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  div,
+  input {
+    height: 30px;
+    width: 150px;
+    border: 2px solid black;
+    border-radius: 5px;
+    background-color: #d2d1d3;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+    margin-right: 20px;
+    flex: 3;
+  }
+  input {
+    width: 100px;
+    flex: 4;
+  }
+  div span {
+    font-weight: 500;
+    font-size: 12px;
+  }
+`;
+
+const Label = styled.div`
   display: flex;
   position: absolute;
-  width: 80px;
+  width: auto;
   height: 20px;
   justify-items: center;
   align-items: center;
@@ -44,15 +77,110 @@ const Viadagem = styled.div`
   left: 20px;
 `;
 
-const SelectInput: React.FC<SelectProps> = ({ value, items, setValue }) => {
+const Plus = styled.button`
+  display: flex;
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+  background-color: #00af54;
+  font-size: 12px;
+  border-radius: 5px;
+  top: -15px;
+  right: -15px;
+  border: none;
+  :active {
+    background-color: rgba(20, 146, 81, 1);
+  }
+`;
+
+interface RemoveProp {
+  hidden?: boolean;
+}
+
+const RemoveIcon = styled.button<RemoveProp>`
+  width: 30px;
+  height: 30px;
+  background-color: #e80234;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 5px;
+  :active {
+    background-color: #c30831;
+  }
+  ${props =>
+    props.hidden &&
+    css`
+      background-color: #d8d8d8;
+      cursor: not-allowed;
+      :active {
+        background-color: #d2d1d3;
+      }
+    `}
+`;
+
+const SelectInput: React.FC<SelectProps> = () => {
+  const [countWinners, setCountWinners] = useState(1);
+  const [winnerList, setWinnerList] = useState([40]);
+
+  function handleAddWinner() {
+    if (countWinners < 3) {
+      setCountWinners(old => old + 1);
+      setWinnerList(old => [...old, 0]);
+    }
+  }
+  function handleRemove(idx: number) {
+    if (countWinners >= 1) {
+      setCountWinners(old => old - 1);
+      setWinnerList(old => old.filter((_, i) => i !== idx));
+    }
+  }
+  function handleChangeAwards(idx: number, value: number) {
+    setWinnerList(old => old.map((val, i) => (i === idx ? value : val)));
+  }
+
   return (
     <Container>
       <Select>
-        {items.map(i => (
-          <option value={i}>{i}</option>
-        ))}
+        {winnerList.map((i, idx) =>
+          idx === 0 ? (
+            <InputContainer>
+              <div>
+                <span>{idx + 1}° Colocado</span>
+              </div>
+              <input
+                value={winnerList[idx]}
+                type="number"
+                onChange={e => handleChangeAwards(idx, Number(e.target.value))}
+              />
+              <RemoveIcon hidden>
+                <FiMinus color="white" size={24} />
+              </RemoveIcon>
+            </InputContainer>
+          ) : (
+            <InputContainer>
+              <div>
+                <span>{idx + 1}° Colocado</span>
+              </div>
+              <input
+                value={winnerList[idx]}
+                type="number"
+                onChange={e => handleChangeAwards(idx, Number(e.target.value))}
+              />
+              <RemoveIcon onClick={() => handleRemove(idx)}>
+                <FiMinus color="white" size={24} />
+              </RemoveIcon>
+            </InputContainer>
+          ),
+        )}
       </Select>
-      <Viadagem>Games</Viadagem>
+      <Label>Valor de premiação</Label>
+      <Plus onClick={() => handleAddWinner()}>
+        <FiPlus color="white" size={24} />
+      </Plus>
     </Container>
   );
 };
--- src/pages/CreateChampioship/index.tsx
+++ src/pages/CreateChampioship/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import AwardInput from '../../components/atoms/CreateChampionship/AwardsInput';
 import SelectInput from '../../components/atoms/CreateChampionship/SelectInput';
 
 const Container = styled.div`
@@ -12,18 +13,24 @@ const Container = styled.div`
 `;
 
 const CreateChampionship: React.FC = () => {
+  // Recebe da api
   const [games, setGames] = useState([
     'Principe da persia',
     'gta',
     'needforspeed',
     'Frifas',
   ]);
-  const [gameSelected, setGameSelected] = useState('');
+  const [gameSelected, setGameSelected] = useState(games[0]);
 
   return (
     <Container>
       <span>Hi my name is Vitor</span>
       <SelectInput
+        value={gameSelected}
+        items={games}
+        setValue={setGameSelected}
+      />
+      <AwardInput
         value={gameSelected}
         items={games}
         setValue={setGameSelected}
