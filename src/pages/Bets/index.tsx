12cca83063982af0b59bb4452a3dab4c4fc96e09
ebertmota <ebert.mota03@gmail.com>
2021-06-19 21:08:35 -0300

    adding more and bets previous page

---
+++ src/pages/Bets/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from './styles';
+import NavigationMenu from '../../components/molecules/NavigationMenu';
+
+const Bets: React.FC = () => {
+  return (
+    <Container>
+      <div>
+        <h2>Página de Apostas em breve !</h2>
+      </div>
+
+      <NavigationMenu />
+    </Container>
+  );
+};
+
+export default Bets;
